8c1cea5f71c0d92b7e0879e03e0a92ce478a0625
jon <jonbaker85@gmail.com>
2020-03-28 18:44:21 +1100

    upgrade cluster and add influxdb

---
+++ apps/influxdb/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+if ! command -v kubectl > /dev/null; then
+  echo "kubectl command not installed"
+  exit 1
+fi
+
+
+if ! command -v helm > /dev/null; then
+  echo "helm command not installed"
+  exit 1
+fi
+
+echo -n "Creating namespace... "
+kubectl -f _namespace.yml create
+
+echo -n "Installing helm chart... "
+helm upgrade --install influxdb --namespace monitoring \
+-f ./values.yaml \
+influxdata/influxdb
